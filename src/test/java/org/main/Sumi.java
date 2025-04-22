98ce31752c0f332d6a88c5fb5d4828cbb8164b6e
Sumitha <Sumitha30>
2025-04-22 23:27:58 +0530

    Siva changes

--- src/test/java/org/main/Sumi.java
+++ src/test/java/org/main/Sumi.java
@@ -3,6 +3,10 @@ package org.main;
 public class Sumi {
 	public static void main(String[] args) {
 		System.out.println("Sumi");
+		System.out.println("Sumisiva");
+		System.out.println("Sumijega");
+		System.out.println("Sumisumi");
+		System.out.println("Sumibala");
 	}
 
 }
